f1bfb5edb6d4df598528bc6806aac7090da47dfe
Shubham0829 <imshubhamkitian007@gmail.com>
2024-09-10 11:24:24 +0530

    CSS Notes

---
+++ Btech JS/session.js
@@ -0,0 +1,22 @@
+// for(let i = 6 ; i <= 60; i=i+6){
+//     console.log(i);
+    
+// }
+
+
+
+// for(let i = 1 ; i <= 60 ; i++){
+//     if(i %2 == 0 && i % 3 ==0){
+//         console.log("6 * ",i/6, " = ", i);  
+//     }
+// }
+
+
+let n = 7;
+let pro = 1;
+for(let i = n ; i >= 1 ;i--){
+    console.log( "line NO:18", pro, i);
+    pro = pro * i
+}
+
+console.log(pro);
--- script.js
+++
@@ -1,2 +0,0 @@
-let p = document.getElementsByTagName('input')
-console.log(p);
